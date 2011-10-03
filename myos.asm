4bec89a7352fffdbd5d15ec8a2e808836376f16a
Christopher Broome <ChrisBroome7@gmail.com>
2011-10-03 01:44:53 -0400

    Removed the read keyboard loop in favor of halting the processor and waking up only for interrupts.

--- myos.asm
+++ myos.asm
@@ -7,7 +7,9 @@
 %define STACK_SPACE		0x1000
 %define STACK_PARAGRAPH_OFFSET	(STACK_SPACE + 512) / 16
 
+	jmp start
 start:
+	cli			; Clear hardware interrupts
 	mov ax, BIOS_START	; The BIOS loads code here
 	mov ds, ax		; Set data segment to where we're loaded
 	;mov cs, ax		; Set code segment to where we're loaded
@@ -15,6 +17,11 @@ start:
 	add ax, STACK_PARAGRAPH_OFFSET ; (4096 + 512) / 16 bytes per paragraph
 	mov ss, ax		; Set stack segment
 	mov sp, STACK_SPACE	; Allocate stack space
+	sti			; Reenable hardware interrupts
+.os_loop:
+	hlt			; Halt the processor and wait for hardware interrupts
+	jmp .os_loop		; Jump back up and halt the processor again
+
 
 read_character_start:
 	xor ax, ax		; zero out ax
@@ -29,18 +36,18 @@ read_character_start:
 	int INT_VID		; call the video interrupt
 	jmp .read_character	; instead of using CPU cycles at idle, wait for keyboard input
 
+
 print_string:			; Routine: output string in SI to screen
 	mov ah, INT_VID_PUT_STRING	; int 10h 'print char' function
-
 .repeat:
 	lodsb			; Get character from string
 	cmp al, 0		;
 	je .done		; If char is zero, end of string
 	int INT_VID		; Otherwise, print it
 	jmp .repeat		;
-
 .done:
 	ret
 
+pad_boot:
 	times 510-($-$$) db 0	; Pad remainder of boot sector with 0s
 	dw 0xAA55		; The standard PC boot signature
