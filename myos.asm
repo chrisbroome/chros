d95e8f9b08f6818092824133cd4308be1f104f97
Christopher Broome <ChrisBroome7@gmail.com>
2011-10-24 22:44:39 -0400

    Not sure what's going on... still.  Might try starting over and switching to BOCHS instead of qemu for debugging.

--- myos.asm
+++ myos.asm
@@ -8,6 +8,10 @@
 ; starts at 0x0000 ends at 0x03ff, 4 bytes per entry, provides argument for jmp instruction
 %define INTERRUPT_VECTOR_TABLE  0x0000
 %define IVT_ENTRY(n)            (n << 2)
+%define IVT_ENTRY_SEGMENT(n)    (IVT_ENTRY(n)+2)
+%define IVT_ENTRY_OFFSET(n)     (IVT_ENTRY(n))
+%define IVT_PTR_SEGMENT(n)      (n+2)
+%define IVT_PTR_OFFSET(n)       (n)
 
 [BITS 16]
     jmp BIOS_START:0x0000+start    ; make this a far jump so that cs:ip is set to 0x07c0:0x0000+start
@@ -23,25 +27,86 @@ start:
     mov sp, STACK_SPACE             ; Allocate stack space
 ; code for setting up the keyboard handler
 
+;    sti
+;    mov al, VIDEO_MODE_TEXT_80x25
+;    mov ah, INT_VID_SET_VIDEO_MODE
+;    int INT_VID
+;    cli
 ; hook the keyboard event interrupt (key_up or key_down)
-    mov ax, word [es:(IVT_ENTRY(INT_KEYEVENT)+2)]   ; read the segment part of the address
-    mov word [old_kb_handler+2], ax                 ; store the segment part of the address
-    call print_hex_16                               ; 
-    mov al, ':'                                     ; 
-    call print_char                                 ; 
 
-    mov ax, word [es:(IVT_ENTRY(INT_KEYEVENT))]     ; read the offset part of the address
-    mov word [old_kb_handler], ax                   ; store the offset part of the address 
-    call print_hex_16                               ; 
+;    mov ax, word [es:(IVT_ENTRY(INT_KEYEVENT)+2)]   ; read the segment part of the address
+;    mov word [old_kb_handler+2], ax                 ; store the segment part of the address
+;    call print_hex_16                               ; 
+;    mov al, ':'                                     ; 
+;    call print_char                                 ; 
+
+;    mov ax, word [es:(IVT_ENTRY(INT_KEYEVENT))]     ; read the offset part of the address
+;    mov word [old_kb_handler], ax                   ; store the offset part of the address 
+;    call print_hex_16                               ; 
     
-    lds si, [new_kb_handler]                        ; load [ds:si] with the new keyboard handler
-    mov di, IVT_ENTRY(INT_KEYEVENT)                 ; load [es:di] with the address of the appropriate interrupt vector
-    mov cx, 4                                       ; we'll be copying 4 bytes
-    cld                                             ; clear the direction flag
-    rep movsb                                       ; move the bytes at [ds:si] to [es:di]
+;    lds si, [new_kb_handler]                        ; load [ds:si] with the new keyboard handler
+;    mov di, IVT_ENTRY(INT_KEYEVENT)                 ; load [es:di] with the address of the appropriate interrupt vector
+;    mov cx, 4                                       ; we'll be copying 4 bytes
+;    cld                                             ; clear the direction flag
+;    rep movsb                                       ; move the bytes at [ds:si] to [es:di]
+    
+;    call print_newline
+
+    call print_regs
+    call print_newline
+; print ds:si
+    push ds
+    push es
+    lds si, [es:IVT_ENTRY(INT_KEYEVENT)]    ; es still at 0x0000
+    mov ax, ds
+    mov word [es:IVT_PTR_SEGMENT(old_kb_handler)], ax ; store the segment
+    mov ax, si
+    mov word [es:IVT_PTR_OFFSET(old_kb_handler)], ax ; store the offset
+    call print_regs
+    call print_newline
+    lds si, [es:new_kb_handler]
+    mov ax, ds
+    mov word [es:IVT_ENTRY_SEGMENT(INT_KEYEVENT)], ax ; store the segment
+    mov ax, si
+    mov word [es:IVT_ENTRY_OFFSET(INT_KEYEVENT)], ax ; store the offset
+    call print_regs
+    call print_newline
+
+    pop es
+    pop ds
+    call print_regs
+    call print_newline
+    
+;    lds si, [new_kb_handler]
+    mov ax, word [es:IVT_ENTRY_SEGMENT(INT_KEYEVENT)]   ; es still at 0x0000
+    call print_hex_16
+    mov al, ':'
+    call print_char
+    mov ax, word [es:IVT_ENTRY_OFFSET(INT_KEYEVENT)] ;
+    call print_hex_16
+    mov al, ' '
+    call print_char
+
+    mov ax, word [es:IVT_PTR_SEGMENT(old_kb_handler)]
+    call print_hex_16
+    mov al, ':'
+    call print_char
+    mov ax, word [es:IVT_PTR_OFFSET(old_kb_handler)]
+    call print_hex_16
+    mov al, ' '
+    call print_char
+    
+    mov ax, word [es:IVT_PTR_SEGMENT(new_kb_handler)]
+    call print_hex_16
+    mov al, ':'
+    call print_char
+    mov ax, word [es:IVT_PTR_OFFSET(new_kb_handler)]
+    call print_hex_16
+    mov al, ' '
+    call print_char
     
     call print_newline
-        
+    
     call reset_mouse  ;
     call print_hex_16 ;
 
@@ -56,8 +121,8 @@ start:
     sti                                             ; enable interrupts
 .wtf:
     hlt
-    call print_regs
-    call print_newline
+;    call print_regs
+;    call print_newline
     inc ax
     jmp .wtf
 
@@ -69,13 +134,8 @@ start:
 
 new_kb_handler:
     ;call dword [old_kb_handler]   ; call the old keyboard handler
-    pushf
 ;    call print_hex_16           ; print the scan code and character in hex
-    push ax
-    mov al, '0'
-    call print_char
-    pop ax
-    popf
+    inc bx
     iret                        ; return from the interrupt
 
 ; INT_VID_TTY_OUTPUT              0x0E    
@@ -96,11 +156,14 @@ print_char:
 ; puts the cursor on a newline
 print_newline:
     push ax
+    push bx
+    mov bx, 0x0007
     mov ah, INT_VID_TTY_OUTPUT
     mov al, 0x0a    ; CR
     int INT_VID
     mov al, 0x0d    ; LF
     int INT_VID
+    pop bx
     pop ax
     ret
 
@@ -139,23 +202,111 @@ print_hex_16:
 
 ; print all registers
 print_regs:
-    pusha               ; push all registers onto the stack in the following order: ax, cx, dx, bx, sp, bp, si, di
+    push ax             ; save ax
     call print_hex_16
+    call print_space
     mov ax, bx
     call print_hex_16
+    call print_space
     mov ax, cx
     call print_hex_16
+    call print_space
     mov ax, dx
     call print_hex_16
+    call print_space
     mov ax, sp
     call print_hex_16
+    call print_space
     mov ax, bp
     call print_hex_16
+    call print_space
     mov ax, si
     call print_hex_16
+    call print_space
     mov ax, di
     call print_hex_16
-    popa
+    call print_space
+    mov ax, ds
+    call print_hex_16
+    call print_space
+    mov ax, es
+    call print_hex_16
+    call print_space
+    popa                ; restore ax
+    ret
+
+print_colon:
+    push ax
+    push bx
+    mov bx, 0x0007
+    mov ax, INT_VID_TTY_OUTPUT
+    mov al, ':'
+    int INT_VID
+    pop bx
+    pop ax
+    ret
+
+print_space:
+    push ax
+    push bx
+    mov bx, 0x0007
+    mov ah, INT_VID_TTY_OUTPUT
+    mov al, ' '
+    int INT_VID
+    pop bx
+    pop ax
+    ret
+
+; Sets an interrupt vector
+; mimics the old dos int 21h functionality:
+; input ds:dx = addres of interrupt handling routine
+set_interrupt_vector:
+    push ds
+    push es
+    lds si, [es:IVT_ENTRY(INT_KEYEVENT)]    ; es still at 0x0000
+    mov ax, ds
+    mov word [es:IVT_PTR_SEGMENT(old_kb_handler)], ax ; store the segment
+    mov ax, si
+    mov word [es:IVT_PTR_OFFSET(old_kb_handler)], ax ; store the offset
+    lds si, [es:new_kb_handler]
+    mov ax, ds
+    mov word [es:IVT_ENTRY_SEGMENT(INT_KEYEVENT)], ax ; store the segment
+    mov ax, si
+    mov word [es:IVT_ENTRY_OFFSET(INT_KEYEVENT)], ax ; store the offset
+    pop es
+    pop ds
+    ret
+
+; get an interrupt vector
+; parameters:       bp+4 vector number
+; return values:    bp+6 segment of interrupt vector
+;                   bp+8 offset of interrupt vector
+; returns an interrupt vector in the variable
+; returns es:bx = interrupt vector
+get_interrupt_vector:
+    enter 0, 0
+    push ax
+    push ds
+    push es
+    xor ax, ax
+    mov es, ax
+    les bx, [es:IVT_ENTRY(INT_KEYEVENT)]    ; es still at 0x0000
+    mov ax, ds
+    mov word [es:IVT_PTR_SEGMENT(old_kb_handler)], ax ; store the segment
+    mov ax, si
+    mov word [es:IVT_PTR_OFFSET(old_kb_handler)], ax ; store the offset
+    lds si, [es:new_kb_handler]
+    mov ax, ds
+    mov word [es:IVT_ENTRY_SEGMENT(INT_KEYEVENT)], ax ; store the segment
+    mov ax, si
+    mov word [es:IVT_ENTRY_OFFSET(INT_KEYEVENT)], ax ; store the offset
+    pop es
+    pop ds
+    pop ax
+    leave
+    ret
+
+save_interupt_vector:
     ret
 
 ; resets the mouse (call before calling any other mouse routine)
