8ec4434aaeb2176d15d4d56394e387b203a05a36
Christopher Broome <ChrisBroome7@gmail.com>
2011-10-26 02:51:34 -0400

    Added routine to dump interrupt vector table.  Working on hooking interrupts

--- myos.asm
+++ myos.asm
@@ -13,6 +13,26 @@
 %define IVT_PTR_SEGMENT(n)      (n+2)
 %define IVT_PTR_OFFSET(n)       (n)
 
+%define BIOS_EQUIP_LIST		0x0410	; (word) BIOS equipment list data - bits 4-5 are 10 for color, 11 for mono
+%define BIOS_KEY_SHIFT_STATUS	0x0417	; (byte) BIOS shift key status
+%define BIOS_KEY_HEAD_PTR	0x041a	; (word) offset of keyboard buffer HEAD pointer
+%define BIOS_KEY_TAIL_PTR	0x041c	; (word) offset of keyboard buffer TAIL pointer
+%define BIOS_KEY_INPUT_QUEUE	0x041e	; (words) from 0x041e to 0x043c keyboard input queue in ASCII-scan code pairs
+%define BIOS_CURRENT_VIDEO_MODE	0x0449	; (byte) current video mode (returned by int 10h, al=0fh)
+%define BIOS_SCREEN_WIDTH	0x044a	; (word) screen width in characters
+%define BIOS_VIDEO_PAGE_NUMBER	0x0462	; (byte) current display page number (returned by int 10h, al=0fh)
+%define BIOS_TICKS_SINCE_MIDNIGHT	0x046c; (dword) time in ticks since midnight (approximately 18.2 ticks/second)
+%define BIOS_CLOCK_PASSED_MIDNIGHT	0x0470; (byte) set to 1 when clock (above) passes midnight
+
+%define INT_RTC		0x08 ; the real time clock interrupt (fires 18.2 times per second)
+
+%macro _print_char 1
+    push ax
+    mov al, %1
+    call print_char
+    pop ax
+%endmacro
+
 [BITS 16]
     jmp BIOS_START:0x0000+start    ; make this a far jump so that cs:ip is set to 0x07c0:0x0000+start
     ; on entry, the dl register will contain number we are booting from
@@ -25,105 +45,30 @@ start:
     add ax, STACK_PARAGRAPH_OFFSET  ; (4096 + 512) / 16 bytes per paragraph
     mov ss, ax                      ; Set stack segment
     mov sp, STACK_SPACE             ; Allocate stack space
-; code for setting up the keyboard handler
 
-;    sti
-;    mov al, VIDEO_MODE_TEXT_80x25
-;    mov ah, INT_VID_SET_VIDEO_MODE
-;    int INT_VID
-;    cli
-; hook the keyboard event interrupt (key_up or key_down)
-
-;    mov ax, word [es:(IVT_ENTRY(INT_KEYEVENT)+2)]   ; read the segment part of the address
-;    mov word [old_kb_handler+2], ax                 ; store the segment part of the address
-;    call print_hex_16                               ; 
-;    mov al, ':'                                     ; 
-;    call print_char                                 ; 
-
-;    mov ax, word [es:(IVT_ENTRY(INT_KEYEVENT))]     ; read the offset part of the address
-;    mov word [old_kb_handler], ax                   ; store the offset part of the address 
-;    call print_hex_16                               ; 
-    
-;    lds si, [new_kb_handler]                        ; load [ds:si] with the new keyboard handler
-;    mov di, IVT_ENTRY(INT_KEYEVENT)                 ; load [es:di] with the address of the appropriate interrupt vector
-;    mov cx, 4                                       ; we'll be copying 4 bytes
-;    cld                                             ; clear the direction flag
-;    rep movsb                                       ; move the bytes at [ds:si] to [es:di]
-    
-;    call print_newline
-
-    call print_regs
-    call print_newline
-; print ds:si
-    push ds
-    push es
-    lds si, [es:IVT_ENTRY(INT_KEYEVENT)]    ; es still at 0x0000
-    mov ax, ds
-    mov word [es:IVT_PTR_SEGMENT(old_kb_handler)], ax ; store the segment
-    mov ax, si
-    mov word [es:IVT_PTR_OFFSET(old_kb_handler)], ax ; store the offset
-    call print_regs
-    call print_newline
-    lds si, [es:new_kb_handler]
-    mov ax, ds
-    mov word [es:IVT_ENTRY_SEGMENT(INT_KEYEVENT)], ax ; store the segment
-    mov ax, si
-    mov word [es:IVT_ENTRY_OFFSET(INT_KEYEVENT)], ax ; store the offset
-    call print_regs
+; print values of all entries in the interrupt vector table
+    mov ax, 0x0040 ; parameter for print_ivt_entries (print 40 entries)
+    call print_ivt_entries
     call print_newline
 
-    pop es
-    pop ds
-    call print_regs
-    call print_newline
-    
-;    lds si, [new_kb_handler]
-    mov ax, word [es:IVT_ENTRY_SEGMENT(INT_KEYEVENT)]   ; es still at 0x0000
-    call print_hex_16
-    mov al, ':'
-    call print_char
-    mov ax, word [es:IVT_ENTRY_OFFSET(INT_KEYEVENT)] ;
-    call print_hex_16
-    mov al, ' '
-    call print_char
+; register our clock handler
+    mov al, INT_RTC	; hook the clock intterupt
+    lds bx, [old_clock_handler] ; load ds:bx with the old handler
+    call save_interupt_vector_to_variable ; old_handler now contains the variable
 
-    mov ax, word [es:IVT_PTR_SEGMENT(old_kb_handler)]
-    call print_hex_16
-    mov al, ':'
-    call print_char
-    mov ax, word [es:IVT_PTR_OFFSET(old_kb_handler)]
-    call print_hex_16
-    mov al, ' '
-    call print_char
-    
-    mov ax, word [es:IVT_PTR_SEGMENT(new_kb_handler)]
-    call print_hex_16
-    mov al, ':'
-    call print_char
-    mov ax, word [es:IVT_PTR_OFFSET(new_kb_handler)]
-    call print_hex_16
-    mov al, ' '
-    call print_char
-    
+    lds bx, [old_clock_handler] ; parameter to print_dword_ptr is ds:bx = dword pointer
+    call print_dword_ptr
     call print_newline
-    
-    call reset_mouse  ;
-    call print_hex_16 ;
 
+    mov ax, 0x0040
+    call print_ivt_entries
     call print_newline
-    
-    mov ax, bx          ; number of mouse buttons
-    call print_hex_16   ;
-    
-    call show_mouse     ; show the mouse
-    
-    xor ax, ax          ; set ax = 0
+
+
+    xor ax, ax
     sti                                             ; enable interrupts
 .wtf:
     hlt
-;    call print_regs
-;    call print_newline
-    inc ax
     jmp .wtf
 
     sti            ; Reenable hardware interrupts
@@ -135,8 +80,17 @@ start:
 new_kb_handler:
     ;call dword [old_kb_handler]   ; call the old keyboard handler
 ;    call print_hex_16           ; print the scan code and character in hex
-    inc bx
+    pusha
+      inc bx
+    popa
     iret                        ; return from the interrupt
+
+new_clock_handler:
+    pusha
+      call print_regs
+      call print_newline
+    popa
+    iret
 
 ; INT_VID_TTY_OUTPUT              0x0E    
 ;       AH = 0Eh
@@ -144,187 +98,192 @@ new_kb_handler:
 ;       BH = page number
 ;       BL = foreground color (graphics modes only)
 print_char:
-    push ax
-    push bx
-    mov ah, INT_VID_TTY_OUTPUT
-    mov bx, 0x0007
-    int INT_VID
-    pop bx
-    pop ax
+    pusha
+      mov ah, INT_VID_TTY_OUTPUT
+      mov bx, 0x0007
+      int INT_VID
+    popa
     ret
     
 ; puts the cursor on a newline
 print_newline:
-    push ax
-    push bx
-    mov bx, 0x0007
-    mov ah, INT_VID_TTY_OUTPUT
-    mov al, 0x0a    ; CR
-    int INT_VID
-    mov al, 0x0d    ; LF
-    int INT_VID
-    pop bx
-    pop ax
+    pusha
+      mov bx, 0x0007
+      mov ah, INT_VID_TTY_OUTPUT
+      mov al, 0x0a    ; CR
+      int INT_VID
+      mov al, 0x0d    ; LF
+      int INT_VID
+    popa
     ret
 
 ; ax = number to write to the screen
 print_hex_16:
-    push ax
-    push bx
-    push cx
-    push dx
-    mov bx, 0x0007
-    mov cx, 0x0010 ; shift amount (must be a multiple of 4)
-    mov dx, 0xf000 ; mask
+    pusha
+      mov bx, 0x0007
+      mov cx, 0x0010 ; shift amount (must be a multiple of 4)
+      mov dx, 0xf000 ; mask
 .while_cx_not_zero:
-    push ax
-    sub cl, 4   ; subtract 4 from cl on each pass
-    and ax, dx  ; mask out the appropriate digit
-    shr ax, cl  ; shift ax right cl times
+      push ax
+	sub cl, 4   ; subtract 4 from cl on each pass
+	and ax, dx  ; mask out the appropriate digit
+	shr ax, cl  ; shift ax right cl times
 .correct_digit:
-    add al, '0'                 ; correct digit for display
-    cmp al, '9'                 ; is digit a number?
-    jle .output                 ; digit is a number
-    add al, 'a'-('9' + 1)       ; digit is a letter
+	add al, '0'                 ; correct digit for display
+	cmp al, '9'                 ; is digit a number?
+	jle .output                 ; digit is a number
+	add al, 'a'-('9' + 1)       ; digit is a letter
 .output:
-    mov ah, INT_VID_TTY_OUTPUT  ; output the character
-    int INT_VID                 ; print the digit
-    pop ax                      ; restore ax
-    shr dx, 4                   ; shift the mask right 4 times
-    jcxz .end                   ; exit if cx == 0
-    jmp .while_cx_not_zero      ; otherwise, do another iteration
+	mov ah, INT_VID_TTY_OUTPUT  ; output the character
+	int INT_VID                 ; print the digit
+      pop ax                      ; restore ax
+      shr dx, 4                   ; shift the mask right 4 times
+      jcxz .end                   ; exit if cx == 0
+      jmp .while_cx_not_zero      ; otherwise, do another iteration
 .end:
-    pop dx
-    pop cx
-    pop bx
-    pop ax
+    popa
     ret
+
+; prints a dword ptr variable to the screen formatted as ffff:ffff
+; ds is implicitly the segment
+; variable offset is in bx
+print_dword_ptr:
+    pusha
+      mov ax, [ds:bx+2] ; segment part of the address
+      call print_hex_16 ; print the segment 
+      _print_char ':'
+      mov ax, [ds:bx] ; offset part of the address
+      call print_hex_16 ; print the offset
+    popa
+    ret
+
 
 ; print all registers
 print_regs:
     push ax             ; save ax
-    call print_hex_16
-    call print_space
-    mov ax, bx
-    call print_hex_16
-    call print_space
+      push ax ; save ax again
+	_print_char 'A'
+      pop ax ; restore ax
+      call print_hex_16
+      _print_char 'B'
+      mov ax, bx
+      call print_hex_16
+      _print_char 'C'
+      mov ax, cx
+      call print_hex_16
+      _print_char 'D'
+      mov ax, dx
+      call print_hex_16
+      _print_char 'S'
+      mov ax, sp
+      call print_hex_16
+      _print_char 'B'
+      mov ax, bp
+      call print_hex_16
+      _print_char 'S'
+      mov ax, si
+      call print_hex_16
+      _print_char 'D'
+      mov ax, di
+      call print_hex_16
+      _print_char 'D'
+      mov ax, ds
+      call print_hex_16
+      _print_char 'E'
+      mov ax, es
+      call print_hex_16
+    pop ax             ; restore ax
+    ret
+
+; prints the value at the specified interrupt vector table entry
+; parameters: ax = interrupt number (only al used as input, but ax destroyed)
+print_ivt_entry:
+    push ax
+      push es
+	push di
+	  call get_interrupt_handler
+	  mov ax, es
+	  call print_hex_16
+	  _print_char ':'
+	  mov ax, di
+	  call print_hex_16
+	pop di
+      pop es
+    pop ax
+    ret
+
+; prints the first al ivt entries
+; parameters: ax = how many entries to print
+print_ivt_entries:
+    push ax
+    push cx
+    ; print values of all entries in the interrupt vector table
+    xor cx, cx          ; set cx = 0
+    mov ch, al          ; set ch = al
+.ivt_entries:
     mov ax, cx
-    call print_hex_16
-    call print_space
-    mov ax, dx
-    call print_hex_16
-    call print_space
-    mov ax, sp
-    call print_hex_16
-    call print_space
-    mov ax, bp
-    call print_hex_16
-    call print_space
-    mov ax, si
-    call print_hex_16
-    call print_space
-    mov ax, di
-    call print_hex_16
-    call print_space
-    mov ax, ds
-    call print_hex_16
-    call print_space
-    mov ax, es
-    call print_hex_16
-    call print_space
-    popa                ; restore ax
+    call print_ivt_entry ; destroys ax
+    _print_char ' '
+    inc cx
+    cmp cl, ch           ; display the first ch intterupt handler addresses
+    jne .ivt_entries
+    pop cx
+    pop ax
     ret
+    
 
-print_colon:
+; get an interrupt handler
+; parameters: ax = interrupt number (only al used as input)
+; returns es:di = interrupt handler
+; destroys di, es
+get_interrupt_handler:
     push ax
-    push bx
-    mov bx, 0x0007
-    mov ax, INT_VID_TTY_OUTPUT
-    mov al, ':'
-    int INT_VID
-    pop bx
+      push bx
+	cli
+	and ax, 0x00ff ; mask out ah
+	shl ax, 2 ; multiply the interrupt number by 4 to get the memory location
+	mov bx, ax ; bx now contains the offset into memory of the interrupt vector for al
+	xor ax, ax  ; zero out ax so we can set es to 0
+	mov es, ax  ; set es to 0
+	les di, [es:bx] ; es:di = [es:bx]...in C, we'd have int **esbx, *esdi; esdi = *esbx;
+	sti
+      pop bx
     pop ax
     ret
 
-print_space:
-    push ax
-    push bx
-    mov bx, 0x0007
-    mov ah, INT_VID_TTY_OUTPUT
-    mov al, ' '
-    int INT_VID
-    pop bx
-    pop ax
+; saves an intterupt vector to the specified variable
+; parameters: ax = intterupt number
+;             [ds:bx] = far dword pointer to variable
+; returns dword [ds:bx] = old interrupt handler; *dsbx = old handler
+save_interupt_vector_to_variable:
+    pusha
+    call get_interrupt_handler ; es:di contains a dword pointer to the intterupt handler address
+    mov ax, [es:di+2] ; segment part of the address
+    mov [ds:bx+2], ax ; 
+    mov ax, [es:di]   ; offset part of the address
+    mov [ds:bx], ax   ;
+    popa
     ret
 
-; Sets an interrupt vector
-; mimics the old dos int 21h functionality:
-; input ds:dx = addres of interrupt handling routine
-set_interrupt_vector:
-    push ds
-    push es
-    lds si, [es:IVT_ENTRY(INT_KEYEVENT)]    ; es still at 0x0000
-    mov ax, ds
-    mov word [es:IVT_PTR_SEGMENT(old_kb_handler)], ax ; store the segment
-    mov ax, si
-    mov word [es:IVT_PTR_OFFSET(old_kb_handler)], ax ; store the offset
-    lds si, [es:new_kb_handler]
-    mov ax, ds
-    mov word [es:IVT_ENTRY_SEGMENT(INT_KEYEVENT)], ax ; store the segment
-    mov ax, si
-    mov word [es:IVT_ENTRY_OFFSET(INT_KEYEVENT)], ax ; store the offset
-    pop es
-    pop ds
-    ret
-
-; get an interrupt vector
-; parameters:       bp+4 vector number
-; return values:    bp+6 segment of interrupt vector
-;                   bp+8 offset of interrupt vector
-; returns an interrupt vector in the variable
-; returns es:bx = interrupt vector
-get_interrupt_vector:
-    enter 0, 0
-    push ax
-    push ds
-    push es
-    xor ax, ax
-    mov es, ax
-    les bx, [es:IVT_ENTRY(INT_KEYEVENT)]    ; es still at 0x0000
-    mov ax, ds
-    mov word [es:IVT_PTR_SEGMENT(old_kb_handler)], ax ; store the segment
-    mov ax, si
-    mov word [es:IVT_PTR_OFFSET(old_kb_handler)], ax ; store the offset
-    lds si, [es:new_kb_handler]
-    mov ax, ds
-    mov word [es:IVT_ENTRY_SEGMENT(INT_KEYEVENT)], ax ; store the segment
-    mov ax, si
-    mov word [es:IVT_ENTRY_OFFSET(INT_KEYEVENT)], ax ; store the offset
-    pop es
-    pop ds
-    pop ax
-    leave
-    ret
-
-save_interupt_vector:
-    ret
-
-; resets the mouse (call before calling any other mouse routine)
-; returns ax = 0 if mouse is installed, -1 (0xffff) otherwise
-;         bx = number of buttons
-reset_mouse:
-    mov ax, 0x0000
-    int 0x33
-    ret
-
-; shows the mouse cursor
+; replaces an interrupt handler with the specified handler
+; parameters: ax = interrupt number
+;             [ds:si] = far dword pointer to new intterupt handler 
 ; returns nothing
-show_mouse:
-    mov ax, 0x0001
-    int 0x33
+; example:
+;     mov ax, 0x09
+;     lds si, ds:new_handler
+;     call hook_interrupt
+hook_interrupt:
+    call get_interrupt_handler ; es:di contains a dword pointer to the intterupt handler address
+    push cx
+    mov cx, 2
+    cld ; clear the direction flag
+    rep movsw ; [ds:si] = [es:di]
+    pop cx
     ret
 
 old_kb_handler:         dd  0       ; far pointer used to store the original keyboard handler
+old_clock_handler:      dd  0       ; far pointer used to store the old clock handler
 pad_boot:
     times 510-($-$$)    db  0       ; Pad remainder of boot sector with 0s
                         dw  0xAA55  ; The standard PC boot signature
